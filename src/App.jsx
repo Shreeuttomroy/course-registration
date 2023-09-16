ed559633992bcdc33d33a0c4afe85e9998f884e5
Shreeuttomroy <shreeuttomr@gmail.com>
2023-09-16 16:57:05 +0600

    total price,total credit and responsive updated

--- src/App.jsx
+++ src/App.jsx
@@ -9,22 +9,27 @@ function App() {
   const [courses, setCourses] = useState([]);
   const [selectedCourse,setSelected] = useState([]);
   const [reamingTime, setReamingTime] = useState(20);
+  const [totalPrice,setTotalPrice] = useState(0);
+  const [totalCredit,setTotalCridit] = useState(0)
   useEffect(() => {
     fetch('courses.json')
     .then((res)=> res.json())
     .then((data)=> setCourses(data));
   }, []);
   const selectHandler = (course)=>{
-    let countPrice = course.price;
-    let countTime = course.time; 
+    let countPrice = parseInt(course.price);
+    let countTime = course.time;
     const matched = selectedCourse.find(obj=> obj.id === course.id);
-    if (reamingTime>-1) {
+    if (reamingTime>=countTime) {
       if (matched){
         return swal('Already Selected');
       }else{
-        selectedCourse.forEach(c=> countPrice += c.price)
+        selectedCourse.forEach(c=> countPrice += parseInt(c.price))
         const reamingTimeTotal = reamingTime - countTime;
         setReamingTime(reamingTimeTotal);
+        setTotalPrice(countPrice);
+        const totalCredit = 20 - reamingTimeTotal;
+        setTotalCridit(totalCredit)
         const selectCourse = [...selectedCourse, course];
         setSelected(selectCourse);
       }
@@ -35,11 +40,11 @@ function App() {
   return (
     <>
     <h1 className=' text-2xl w-fit mx-auto font-serif font-bold my-6'>Course Registration</h1>
-    <div className=' w-full px-3 flex justify-between gap-2'>
-      <div className='w-3/4 grid md:grid-cols-3 gap-2 justify-between'>
+    <div className=' w-full px-3 flex justify-around flex-wrap md:flex-nowrap md:justify-between gap-2'>
+      <div className=' w-full md:w-3/4 grid grid-cols-1 md:grid-cols-3 gap-2 justify-between'>
         {courses.map((course,idex)=><Course key={idex} selectHandler={selectHandler}  course={course}></Course>)}
       </div>
-      <Selected reamingTime={reamingTime} selectedCourse={selectedCourse}></Selected>
+      <Selected totalCredit={totalCredit} totalPrice={totalPrice} reamingTime={reamingTime} selectedCourse={selectedCourse}></Selected>
     </div>
     </>
   )
--- src/components/Selected.jsx
+++ src/components/Selected.jsx
@@ -1,27 +1,29 @@
 import PropTypes from 'prop-types';
 import Select from './Select';
-const Selected = ({reamingTime, selectedCourse}) => {
+const Selected = ({totalPrice, totalCredit, reamingTime, selectedCourse}) => {
     return ( 
         <>
-        <div className="w-1/4 bg-[#FFFFFF] h-fit rounded-md p-3 mt-3">
-            <h1>Credit Hour Remaining {reamingTime} hr</h1>
+        <div className=" w-full md:w-1/4 bg-[#FFFFFF] h-fit rounded-md p-3 mt-3">
+            <h1 className=' text-lg text-[#2F80ED] font-bold'>Credit Hour Remaining {reamingTime} hr</h1>
             <hr className='w-full my-2'/>
             <div>
-                <h3>Course Name:</h3>
+                <h3 className='text-lg font-bold' >Course Name:</h3>
                 <ul className=' list-decimal ml-4'>
                 {selectedCourse.map(data=> <Select key={data.id} data={data}></Select>)}
                 </ul>
             </div>
             <hr className='w-full my-2'/>
-            <p>Total credit hour:</p>
+            <p className=' font-medium'>Total credit hour: {totalCredit}</p>
             <hr className='w-full my-2'/>
-            <p>Total Cost: </p>
+            <p className=' font-semibold'>Total Cost: {totalPrice} USD</p>
         </div>
         </>
      );
 }
  Selected.propTypes = {
     selectedCourse: PropTypes.array,
-    reamingTime: PropTypes.number
+    reamingTime: PropTypes.number,
+    totalCredit: PropTypes.number,
+    totalPrice: PropTypes.number
  }
 export default Selected;
